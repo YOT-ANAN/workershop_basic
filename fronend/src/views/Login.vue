69083174f5c7d4e9fcc53116fec8bca35872b6ca
YOT-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2022-01-10 15:12:32 +0700

    learn v-show

---
+++ fronend/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ data }}</h1>
+    <h1 v-if="show">{{ show }}</h1>
+    <v-btn color="success" @click="show = !show">switch</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: 'Hello From Login',
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
